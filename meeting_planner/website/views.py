db21a417fc031676d0c3496ede93711880555964
prathish <prathish.bv@freightify.com>
2023-03-13 11:18:22 +0530

    adding meeting to home

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from datetime import datetime
+from meetings.models import Meeting
 
 def welcome(request):
-    return render(request, 'website/home.html')
+    return render(request, 'website/home.html', {'meetings': Meeting.objects.all()})
